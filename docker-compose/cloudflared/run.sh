ed54f88b8b5cc79dfb87a04540a5894ea17ab270
fuongz <hi@phuongphung.com>
2024-06-19 05:22:56 +0000

    feat: add pinchflat

--- docker-compose/cloudflared/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose --env-file ./../../docker.env/.cloudflared.env up -d
--- docker-compose/public_stack/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose --env-file ./../../docker.env/.jellyfin.env up -d
